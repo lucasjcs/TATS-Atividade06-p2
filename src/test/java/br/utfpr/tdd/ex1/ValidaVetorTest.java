59e5424b384c2ffd20e3d7922873311f8d7fdd6d
Lucas Jacinto <silval@alunos.utfpr.edu.br>
2018-03-28 18:30:37 -0300

    Finalizado - Refatorando escrita do assertEquals no teste

--- src/test/java/br/utfpr/tdd/ex1/ValidaVetorTest.java
+++ src/test/java/br/utfpr/tdd/ex1/ValidaVetorTest.java
@@ -15,32 +15,30 @@ public class ValidaVetorTest {
         double v[] = {};
         valida.setVetor(v);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void verificaVetorNuloTest() throws Exception {
         ValidaVetor valida = new ValidaVetor();
         double[] v = null;
         valida.setVetor(v);
     }
-    
-    @Test
-    public void verificaNMenoresValoresTest(){
-        ValidaVetor valida = new ValidaVetor();
-        double[] v = {1,2,3,4,5};
-        ArrayList<Double> vresp = valida.getNMenoresValores(v, 4);
-        assertEquals(vresp.get(0), 1, 0.000001);
-        assertEquals(vresp.get(1), 2, 0.000001);
-        assertEquals(vresp.get(2), 3, 0.000001);
-    }
-    
-    @Test
-    public void verificaNMaioresValoresTest(){
-        ValidaVetor valida = new ValidaVetor();
-        double[] v = {1,2,3,4,5};
-        ArrayList<Double> vresp = valida.getNMaioresValores(v, 3);
-        assertEquals(vresp.get(0), 4, 0.000001);
-        assertEquals(vresp.get(1), 5, 0.000001);
-    }
-    
 
+    @Test
+    public void verificaNMenoresValoresTest() {
+        ValidaVetor valida = new ValidaVetor();
+        double[] v = {1, 2, 3, 4, 5};
+        ArrayList<Double> vresp = valida.getNMenoresValores(v, 4);
+        assertEquals(1, vresp.get(0), 0.000001);
+        assertEquals(2, vresp.get(1), 0.000001);
+        assertEquals(3, vresp.get(2), 0.000001);
+    }
+
+    @Test
+    public void verificaNMaioresValoresTest() {
+        ValidaVetor valida = new ValidaVetor();
+        double[] v = {1, 2, 3, 4, 5};
+        ArrayList<Double> vresp = valida.getNMaioresValores(v, 3);
+        assertEquals(4, vresp.get(0), 0.000001);
+        assertEquals(5, vresp.get(1), 0.000001);
+    }
 }
